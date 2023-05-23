20c08ff48fc9fc3bac63b8b565e6e982be0b0e9e
evandro-rf-dutra <evandrorfdutra@gmail.com>
2023-05-23 14:26:27 -0300

    23_05

--- Pong/DELETAR.js
+++ Pong/DELETAR.js
@@ -1,8 +1,26 @@
+console.log('teste');
 
+var meio_bola = 59;
+var meio_player = 50;
 
-x = 0.5555555555555599999999999;
-console.log(x);
+var velocidade_jogador = 10;
 
-if(x <= 0){
-    console.log('sim');
+var acuracia = meio_bola - meio_player;
+
+console.log(acuracia);
+
+if( (meio_bola - meio_player) < 0 ){ // negativo
+    // valor
+    if( (meio_bola - meio_player) > (velocidade_jogador*(-1))){
+        console.log('salto de precisao');
+        console.log(`${(meio_bola - meio_player)} < ${(velocidade_jogador*(-1))}`);
+    }
+}
+else{
+    // positivo
+    if( (meio_bola - meio_player) < (velocidade_jogador)){
+        console.log('salto de precisao');
+        console.log(`${(meio_bola - meio_player)} < ${(velocidade_jogador)}`);
+    }
+
 }
